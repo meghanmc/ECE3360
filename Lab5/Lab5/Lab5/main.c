a7dd2741a31ab0b1f6f672731ecfd037d0fe00e1
Dylan <dtmcnamara13@gmail.com>
2019-04-04 09:14:12 -0500

    Project setup for Lab 5

---
+++ Lab5/Lab5/Lab5/main.c
@@ -0,0 +1,23 @@
+/*
+ * Lab5.c
+ *
+ * Created: 4/4/2019 9:07:46 AM
+ * Author : Dtmcn
+ */ 
+
+#ifndef F_CPU
+#define F_CPU 8000000UL
+#endif
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
