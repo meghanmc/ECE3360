08eec71a8925fe8682faf3ba769e7c4d8f24f700
Dylan <dtmcnamara13@gmail.com>
2019-04-05 14:15:10 -0500

    Tx worksgit add Lab5/Lab5/Lab5/main.c

--- Lab5/Lab5/Lab5/main.c
+++ Lab5/Lab5/Lab5/main.c
@@ -5,9 +5,8 @@
  * Authors : McLaughlin, Meghan & McNamara, Dylan
  */ 
 
-#ifndef F_CPU
-#define F_CPU 8000000UL
-#endif
+#define F_CPU 8000000L
+
 
 // define baud rate as 9600
 #define BAUD 9600
@@ -17,16 +16,17 @@
 #include <util/delay.h>
 #include <string.h>
 #include <avr/pgmspace.h>
+#include <stdio.h>
 
 // store strings in SRAM and FLASH
-const char sdata[] = "This string is in SRAM";
-const char fdata[] PROGMEM = "This string is in FLASH";
+const char sdata[] = "This string is in SRAM\n";
+const char fdata[] PROGMEM = "This string is in FLASH\n";
 
 // function prototypes
 void usart_init(void);
 void usart_prints(const char *ptr);
 void usart_printf(const char *ptr);
-void usart_putchar(char c);
+void usart_putchar(const char c);
 int readADC(void);
 
 
@@ -38,23 +38,26 @@ int main(void){
 	usart_init();
 	usart_prints(sdata);
 	usart_printf(fdata);
+	usart_prints("This string is defined in main()\n");
+
+	while(1)
+		;
+	return(1);
 }
 
 // initialize the USART
 void usart_init(void){
 	// set baud rate
 	unsigned short ubrr;
-	ubrr = (double)F_CPU / (BAUD*16.0) - 1.0;
-	UBRR0H = (unsigned char) (ubrr & 0xFF00);
-	UBRR0L = (unsigned char) (ubrr & 0x00FF);
-	
-	// enable Rx and Tx
-	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-	
-	// 8 data bits, odd parity, 2 stop bits 
-	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00) | (1<<UPM01) | (1<<UPM00) | (1<<USBS0);
+	ubrr = (double) (F_CPU / (BAUD*16.0) - 1.0);
+	UBRR0H = (ubrr & 0xFF00);
+	UBRR0L = (ubrr & 0x00FF);
 
-	// Shouldn't need to set up DDRD because they are set by default when USART mode is enabled
+	// enable Rx and Tx, Rx complete interrupt enabled
+	UCSR0B = (1<<RXCIE0)|(1<<RXEN0)|(1<<TXEN0);
+
+	//Asynchronous USART, 8 data bits, NO parity, 1 stop bits
+	UCSR0C = (1<<UCSZ01)|(1<<UCSZ00);
 }
 
 // print a string from SRAM
@@ -76,18 +79,32 @@ void usart_printf(const char *ptr){
 		c = pgm_read_byte_near(ptr++);
 		usart_putchar(c);
 	}
-	
 }
 
 // writes a single character of a string
-void usart_putchar(char c){
+void usart_putchar(const char c){
 	// UDRE0 = USART Data Register 0 empty flag
 	// Wait until the data register is ready for data
 	while(!(UCSR0A & (1<<UDRE0)))
 		; //wait for data register to be ready
+	cli();
 	UDR0 = c;
+	sei();
 }
 
+ISR(USART_RX_vect)
+{
+	// UART receive interrupt handler.
+	// To do: check and warn if buffer overflows.
+	/*
+	char c = UDR0;
+	rx_buffer[rx_buffer_head] = c;
+	if (rx_buffer_head == RX_BUFFER_SIZE - 1)
+	rx_buffer_head = 0;
+	else
+	rx_buffer_head++;
+	*/
+}
 
 // read ADC voltage level
 int readADC(){
