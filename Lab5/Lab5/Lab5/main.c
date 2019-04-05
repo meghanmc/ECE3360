eb1199faa22463b0b0391744a322e7cf9b0acfe4
McLaughlin <mlmclaughlin@uiowa.edu>
2019-04-05 11:12:40 -0500

    implemented usart_printf

--- Lab5/Lab5/Lab5/main.c
+++ Lab5/Lab5/Lab5/main.c
@@ -17,31 +17,43 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 #include <string.h>
+#include <avr/pgmspace.h>
+
+// rx_buffer setup
+#define RX_BUFFTER_SIZE 32
+unsigned char rx_buffer[RX_BUFFTER_SIZE];
+unsigned char rx_buffer_head;
+unsigned char rx_buffer_tail;
+
+// store strings in SRAM and FLASH
+const char sdata[] = "This string is stored in SRAM";
+const char fdata[] PROGMEM = "This string is stored in FLASH";
+
+// function prototypes
+void usart_init(void);
+void usart_prints(char *ptr);
+void usart_printf(char *ptr);
+void usart_putchar(char c);
+int readADC(void);
 
 
-
-int main(void)
-{
+int main(void){
 	// enable global interrupts
 	sei();
 	
 	usart_init();
-	//usart_prints(sdata)
-	//usart_printf(fdata)
+	usart_prints(sdata);
+	usart_printf(fdata);
 }
 
 // initialize the USART
-void usart_init()
-{
+void usart_init(void){
 	// set baud rate
 	UBRR0H = (unsigned char) (MYUBRR>>8);
 	UBRR0L = (unsigned char) MYUBRR;
 	
 	// enable Rx and Tx
 	UCSR0B = (1<<RXEN0) | (1<<TXEN0);
-	
-	
-	
 	
 	// 8 data bits, odd parity, 2 stop bits 
 	UCSR0C = (1<<UCSZ01) | (1<<UCSZ00) | (1<<UPM01) | (1<<UPM00) | (1<<USBS0);
@@ -50,21 +62,37 @@ void usart_init()
 }
 
 // print a string from SRAM
-void usart_prints(char sdata[])
-{
-	
+void usart_prints(char *ptr){
+	while (ptr) {
+
+	}
 }
 
 // print a string from FLASH
-void usart_printf(char fdata)
-{
-	// see data sheet pg 160 ish
+void usart_printf(char *ptr){
+	char c;
+	//
+	while(pgm_read_byte_near(ptr)) {
+		c = pgm_read_byte_near(ptr++);
+		usart_putchar(c);
+	}
 	
 }
 
+// writes a single character of a string
+void usart_putchar(char c){
+	// UDRE0 - USART Data Register 0 empty flag
+	// Wait until the data register is ready for data
+	while(!(UCSR0A & (1<<UDRE0)))
+		;
+	UDR0 = c;
+}
+
+void usart_putchar(char c){
+}
+
 // read ADC voltage level
-int readADC()
-{
+int readADC(){
 	
 }
 
