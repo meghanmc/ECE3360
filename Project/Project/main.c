48dfbd96a28344c7608e575ed6c413dcb6c1571d
McLaughlin <mlmclaughlin@uiowa.edu>
2019-04-19 12:32:08 -0500

    Directory for final project.

---
+++ Project/Project/main.c
@@ -0,0 +1,18 @@
+/*
+ * Project.c
+ *
+ * Created: 4/19/2019 12:24:42 PM
+ * Author : mlmclaughlin
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
